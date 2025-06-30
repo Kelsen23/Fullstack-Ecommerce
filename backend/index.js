edbce0c41886ed4b0519d9e1555e57c4331a2b75
Kelsen23 <nikolozigoreziani@gmail.com>
2025-06-30 14:30:04 +0400

    Import router to index.js

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config({ path: path.resolve(process.cwd(), "backend/.env") });
 const port = process.env.PORT || 5000;
@@ -13,9 +14,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/api/users", userRoutes);
 
 app.listen(port, () => console.log(`Server up on port: ${port}`));
