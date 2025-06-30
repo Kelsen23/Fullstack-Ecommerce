0618d774aa93b72d394e161faa4d81f630bdf1c8
Kelsen23 <nikolozigoreziani@gmail.com>
2025-06-30 14:29:43 +0400

    Create controller that creates user if email is available and stores in db

---
+++ backend/controllers/userController.js
@@ -0,0 +1,39 @@
+import User from "../models/userModel.js";
+import asyncHandler from "../middlewares/asynchandler.js";
+import bcrypt from "bcryptjs";
+
+const createUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    const error = new Error("Please fill all the inputs.");
+    error.statusCode = 400;
+    throw error;
+  }
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) res.status(400).send("User already exists");
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  try {
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+export { createUser };
