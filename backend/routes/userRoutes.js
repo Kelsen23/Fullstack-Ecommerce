84c945e18a62e55f5e1ff1061eb16d3269b364f9
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-01 14:24:28 +0400

    Add GET method to api/users  if user has a jwt token and is an admin

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { createUser, loginUser, logoutCurrentUser } from "../controllers/userController.js";
+import { createUser, loginUser, logoutCurrentUser, getAllUsers } from "../controllers/userController.js";
+import { authenticate, authorizedAdmin } from "../middlewares/authMiddleware.js";
+
 const router = express.Router();
 
-router.route("/").post(createUser);
+router.route("/").post(createUser).get(authenticate, authorizedAdmin, getAllUsers);
 router.post("/auth", loginUser);
 router.post("/logout", logoutCurrentUser);
+router
 
 export default router;
