5617d84b42a300158e314becfa8a70cf9e9828ce
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-02 13:28:59 +0400

    Import useSelector, useDispatch from redux. Create logoutHandler function

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -9,14 +9,33 @@ import {
 import { FaHeart } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
 import "./Navigation.css";
+import { useSelector, useDispatch } from "react-redux";
+import { useLoginMutation } from "../../redux/api/usersApiSlice";
+import { logout } from "../../redux/features/auth/authSlice";
 
 const Navigation = () => {
+  const { userInfo } = useSelector((state) => state.auth);
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [showSidebar, setShowSidebar] = useState(false);
 
   const toggleDropdown = () => setDropdownOpen((prev) => !prev);
   const toggleSidebar = () => setShowSidebar((prev) => !prev);
   const closeSidebar = () => setShowSidebar(false);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLoginMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate("/login");
+    } catch (error) {
+      console.error("Logout failed:", error);
+    }
+  };
 
   return (
     <div
@@ -58,6 +77,13 @@ const Navigation = () => {
           <FaHeart className="mr-2 mt-[3rem]" />
           <span className="hidden nav-item-name mt-[3rem]">FAVORITE</span>
         </Link>
+      </div>
+
+      <div className="relative">
+        <button
+          onClick={toggleDropdown}
+          className="flex items-center text-gray-8000 focus:outline-none "
+        ></button>
       </div>
 
       <ul>
