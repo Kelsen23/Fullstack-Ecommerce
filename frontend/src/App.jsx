964fc012f3a3d33a223d80b94655205ed994164b
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-02 10:52:24 +0400

    Set up App.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,18 @@
+import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Navigation from "./pages/Auth/Navigation";
+import "react-toastify/dist/ReactToastify.css";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <ToastContainer />
+      <Navigation />
+      <main className="py-3">
+        <Outlet />
+      </main>
+    </div>
+  );
 };
 
 export default App;
