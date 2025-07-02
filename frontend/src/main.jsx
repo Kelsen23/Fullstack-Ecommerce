8487ad337cd78c1e93f43cdf53f8d549b5b6039e
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-02 14:59:13 +0400

    Wrap entire application in redux Provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,11 +7,15 @@ import {
 } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
 const router = createBrowserRouter(
   createRoutesFromElements(<Route path="/" element={<App />} />)
 );
 
 createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
