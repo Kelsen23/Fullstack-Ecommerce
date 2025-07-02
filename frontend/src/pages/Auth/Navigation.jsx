bbd127381151e69deb7d936853d8c44e66b1ebcd
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-02 11:38:34 +0400

    Create Navigation

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -1,5 +1,88 @@
+import { useState } from "react";
+import {
+  AiOutlineHome,
+  AiOutlineShopping,
+  AiOutlineLogin,
+  AiOutlineUserAdd,
+  AiOutlineShoppingCart,
+} from "react-icons/ai";
+import { FaHeart } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import "./Navigation.css";
+
 const Navigation = () => {
-  return <div>Navigation</div>;
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const toggleDropdown = () => setDropdownOpen((prev) => !prev);
+  const toggleSidebar = () => setShowSidebar((prev) => !prev);
+  const closeSidebar = () => setShowSidebar(false);
+
+  return (
+    <div
+      style={{ zIndex: 999 }}
+      className={`${
+        showSidebar ? "hidden" : "flex"
+      } xl:flex lg:flex md:hidden sm:hidden flex-col justify-between p-4 bg-black text-white w-[4%] hover:[15%] h-[100vh] fixed`}
+      id="navigation-container"
+    >
+      <div className="flex flex-col justify-center ">
+        <Link
+          to="/"
+          className="flex items-center transition-transform transform hover:translate-x-2"
+        >
+          <AiOutlineHome className="mr-2 mt-[3rem]" />
+          <span className="hidden nav-item-name mt-[3rem]">HOME</span>
+        </Link>
+
+        <Link
+          to="/shop"
+          className="flex items-center transition-transform transform hover:translate-x-2"
+        >
+          <AiOutlineShopping className="mr-2 mt-[3rem]" />
+          <span className="hidden nav-item-name mt-[3rem]">SHOP</span>
+        </Link>
+
+        <Link
+          to="/cart"
+          className="flex items-center transition-transform transform hover:translate-x-2"
+        >
+          <AiOutlineShoppingCart className="mr-2 mt-[3rem]" />
+          <span className="hidden nav-item-name mt-[3rem]">CART</span>
+        </Link>
+
+        <Link
+          to="/favorite"
+          className="flex items-center transition-transform transform hover:translate-x-2"
+        >
+          <FaHeart className="mr-2 mt-[3rem]" />
+          <span className="hidden nav-item-name mt-[3rem]">FAVORITE</span>
+        </Link>
+      </div>
+
+      <ul>
+        <li>
+          <Link
+            to="/login"
+            className="flex items-center transition-transform transform hover:translate-x-2"
+          >
+            <AiOutlineLogin className="mr-2 mt-[3rem]" />
+            <span className="hidden nav-item-name mt-[3rem]">LOG IN</span>
+          </Link>
+        </li>
+
+        <li>
+          <Link
+            to="/register"
+            className="flex items-center transition-transform transform hover:translate-x-2"
+          >
+            <AiOutlineUserAdd className="mr-2 mt-[3rem]" />
+            <span className="hidden nav-item-name mt-[3rem]">REGISTER</span>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navigation;
