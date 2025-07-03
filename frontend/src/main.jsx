67b628f0279ad7a6eab7bcc49a92927d85a7c36f
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-03 17:03:44 +0400

    Initialize Profile component route with PrivateRoute protecting it from accessing if client isn't logged in

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,14 +7,25 @@ import {
 } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
-import { Provider } from "react-redux"; 
+import { Provider } from "react-redux";
 import store from "./redux/store.js";
 import Login from "./pages/Auth/Login.jsx";
 import Register from "./pages/Auth/Register.jsx";
+import Profile from "./pages/User/Profile.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
+      <Route
+        path="/profile"
+        element={
+          <PrivateRoute>
+            <Profile />
+          </PrivateRoute>
+        }
+      />
+
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
     </Route>
