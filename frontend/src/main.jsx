5c7288e74483198db288904a64097767c24fdf26
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-03 10:36:49 +0400

    Create a route for login component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,20 @@ import {
   RouterProvider,
   createRoutesFromElements,
   createBrowserRouter,
+  Routes,
 } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
+import Login from "./pages/Auth/Login.jsx";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<App />} />)
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/login" element={<Login />} />
+    </Route>
+  )
 );
 
 createRoot(document.getElementById("root")).render(
