e7a052c001059e9bcc5b2a91f6a0566413fbef1c
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-03 16:30:22 +0400

    Add Images

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -135,6 +135,12 @@ const Register = () => {
           </p>
         </div>
       </div>
+
+      <img
+        src="https://images.unsplash.com/photo-1576502200916-3808e07386a5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2065&q=80"
+        alt=""
+        className="h-[40rem] w-[40rem] xl:block md:hidden sm:hidden rounded-lg pr-3"
+      />
     </section>
   );
 };
