367556e707d2aafacda7cf778f2b7aa874b99ea0
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-04 15:50:11 +0400

    Render users in table

---
+++ frontend/src/pages/Admin/UserList.jsx
@@ -0,0 +1,129 @@
+import { useState, useEffect } from "react";
+import { FaTrash, FaEdit, FaCheck, FaTimes } from "react-icons/fa";
+import Loader from "../../components/Loader";
+import { toast } from "react-toastify";
+import {
+  useGetUsersQuery,
+  useDeleteUserMutation,
+  useGetUserDetailsQuery,
+  useUpdateUserMutation,
+} from "../../redux/api/usersApiSlice";
+import Message from "../../components/Message";
+
+const UserList = () => {
+  const { data: users, refetch, isLoading, error } = useGetUsersQuery();
+  const [deleteUser] = useDeleteUserMutation();
+
+  const [editableUserId, setEditableUserId] = useState(null);
+  const [editableUsername, setEditableUsername] = useState("");
+  const [editableUserEmail, setEditableUserEmail] = useState("");
+
+  const [updateUser] = useUpdateUserMutation();
+
+  useEffect(() => {
+    refetch();
+  }, [refetch]);
+
+  return (
+    <div className="ml-[5rem] p-4">
+      <h1 className="text-2xl font-semibold mb-4">Users</h1>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="error">
+          {error?.data.message || error.message}
+        </Message>
+      ) : (
+        <div className="flex flex-col md:flex-row">
+          <table className="w-full md:w-4/5 mx-auto">
+            <thead>
+              <tr>
+                <th className="px-4 py-2 text-left">ID</th>
+                <th className="px-4 py-2 text-left">NAME</th>
+                <th className="px-4 py-2 text-left">EMAIL</th>
+                <th className="px-4 py-2 text-left">ADMIN</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {users?.map((user) => (
+                <tr key={user._id}>
+                  <td className="px-4 py-2">{user._id}</td>
+                  <td className="px-4 py-2">
+                    {editableUserId === user._id ? (
+                      <div className="flex items-center">
+                        <input
+                          type="text"
+                          value={editableUsername}
+                          onChange={(e) => setEditableUsername(e.target.value)}
+                          className="w-full p-2 border rounded-lg"
+                        />
+                        <button
+                          onClick={() => updateHandler(user._id)}
+                          className="ml-2 bg-blue-500 text-white py-2 px-4 rounded-lg"
+                        >
+                          <FaCheck />
+                        </button>
+                      </div>
+                    ) : (
+                      <div className="flex items-center">
+                        {user.username}{" "}
+                        <button
+                          onClick={() =>
+                            toggleEdit(user._id, user.username, user.email)
+                          }
+                          className="cursor-pointer"
+                        >
+                          <FaEdit className="ml-[1rem]" />
+                        </button>
+                      </div>
+                    )}
+                  </td>
+                  <td className="px-4 py-2">
+                    {editableUserId === user._id ? (
+                      <div className="flex items-center">
+                        <input
+                          type="text"
+                          value={editableUserEmail}
+                          onChange={(e) => setEditableUserEmail(e.target.value)}
+                          className="w-full p-2 border rounded-lg"
+                        />
+                        <button
+                          onClick={(e) => updateHandler(user._id)}
+                          className="ml-2 bg-blue-500 text-white py-2 px-4 rounded-lg"
+                        >
+                          <FaCheck />
+                        </button>
+                      </div>
+                    ) : (
+                      <div className="flex items-center">
+                        {user.email}{" "}
+                        <button
+                          onClick={() =>
+                            toggleEdit(user._id, user.username, user.email)
+                          }
+                          className="cursor-pointer"
+                        >
+                          <FaEdit className="ml-[1rem]" />
+                        </button>
+                      </div>
+                    )}
+                  </td>
+                  <td className="px-4 py-2">
+                    {user.isAdmin ? (
+                      <FaCheck style={{ color: "green" }} />
+                    ) : (
+                      <FaTimes style={{ color: "red" }} />
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UserList;
