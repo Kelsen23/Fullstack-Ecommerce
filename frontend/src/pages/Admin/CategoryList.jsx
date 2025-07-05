289c1571eebec1336c9fec53ade7518c4a25282c
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-05 18:28:35 +0400

    Create CategoryList with handleCreate logic, register CategoryForm component in there and render categories from db

---
+++ frontend/src/pages/Admin/CategoryList.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import { toast } from "react-toastify";
+import {
+  useCreateCategoryMutation,
+  useUpdateCategoryMutation,
+  useDeleteCategoryMutation,
+  useFetchCategoriesQuery,
+} from "../../redux/api/categoryApiSlice";
+import CategoryForm from "../../components/CategoryForm";
+
+const CategoryList = () => {
+  const { data: categories, refetch } = useFetchCategoriesQuery();
+  const [name, setName] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [updatedName, setUpdatedName] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const [createCategory] = useCreateCategoryMutation();
+  const [updateCategory] = useUpdateCategoryMutation();
+  const [deleteCategory] = useDeleteCategoryMutation();
+
+  const handleCreateCategory = async (e) => {
+    e.preventDefault();
+
+    if (!name) {
+      toast.error("Category name is required!");
+      return;
+    }
+
+    try {
+      const res = await createCategory({ name }).unwrap();
+      refetch();
+      toast.success(`Successfully created category with name of: ${res.name}`);
+    } catch (error) {
+      console.error(error);
+      toast.error(error?.data?.error || error?.error || "Register failed");
+    }
+  };
+
+  return (
+    <div className="ml-[10rem] flex flex-col md:flex-row">
+      <div className="md:w-3/4 p-3">
+        <div className="h-12">Manage Categories</div>
+
+        <CategoryForm
+          value={name}
+          setValue={setName}
+          handleSubmit={handleCreateCategory}
+        />
+        <br />
+        <hr />
+
+        <div className="flex flex-wrap">
+          {categories?.map((category) => (
+            <div key={category._id}>
+              <button
+                onClick={() => {
+                  setModalVisible(true);
+                  setSelectedCategory(category);
+                  setUpdatedName(category.name);
+                }}
+                className="bg-white border-pink-500 text-pink-500 py-2 px-4 rounded-lg m-3 hover:bg-pink-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50"
+              >
+                {category.name}
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryList;
