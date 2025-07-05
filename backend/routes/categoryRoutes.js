7839b1574650674d993097fe270dcb26f55c63f7
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-05 14:01:42 +0400

    Register updateCategory controller with the route of api/category/:categoryId

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -3,9 +3,13 @@ import {
   authenticate,
   authorizedAdmin,
 } from "../middlewares/authMiddleware.js";
-import { createCategory } from "../controllers/categoryController.js";
+import {
+  createCategory,
+  updateCategory,
+} from "../controllers/categoryController.js";
 const router = express.Router();
 
 router.route("/").post(authenticate, authorizedAdmin, createCategory);
+router.route("/:categoryId").put(authenticate, authorizedAdmin, updateCategory);
 
 export default router;
