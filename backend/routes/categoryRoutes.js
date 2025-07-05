42c7e3452f13d8ae3119b1ed823906cc98e4ae9e
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-05 13:03:31 +0400

    Authenticate user before creating category

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -6,6 +6,6 @@ import {
 import { createCategory } from "../controllers/categoryController.js";
 const router = express.Router();
 
-router.route("/").post(createCategory);
+router.route("/").post(authenticate, createCategory);
 
 export default router;
