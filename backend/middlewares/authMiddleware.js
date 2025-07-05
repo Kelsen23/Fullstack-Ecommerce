9cf9d81277b3c87c239a813cf441e273c470a6e6
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-05 15:12:25 +0400

    Update status code for not authorized admin

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -28,7 +28,7 @@ const authorizedAdmin = (req, res, next) => {
   if (req.user && req.user.isAdmin) {
     next();
   } else {
-    res.status(401).json({ error: "Not authorized as an admin." });
+    res.status(403).json({ error: "Not authorized as an admin." });
   }
 };
 
