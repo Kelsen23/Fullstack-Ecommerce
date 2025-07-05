3a3b199ba45f4f792fb53cbbc39c76737444eaf3
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-05 14:49:07 +0400

    Add route for listCategory

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -7,11 +7,15 @@ import {
   createCategory,
   updateCategory,
   deleteCategory,
+  listCategory,
 } from "../controllers/categoryController.js";
 const router = express.Router();
 
 router.route("/").post(authenticate, authorizedAdmin, createCategory);
 router.route("/:categoryId").put(authenticate, authorizedAdmin, updateCategory);
-router.route("/:categoryId").delete(authenticate, authorizedAdmin, deleteCategory);
+router
+  .route("/:categoryId")
+  .delete(authenticate, authorizedAdmin, deleteCategory);
+router.route("/categories").get(listCategory);
 
 export default router;
