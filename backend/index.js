896753de7a3f770196ddc44a3be30c93a4a31fa1
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-05 12:06:51 +0400

    Register categoryRoutes

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
+import categotyRoutes from "./routes/categoryRoutes.js";
 
 dotenv.config({ path: path.resolve(process.cwd(), "backend/.env") });
 const port = process.env.PORT || 5000;
@@ -15,5 +16,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/api/users", userRoutes);
+app.use("/api/category", categotyRoutes);
 
 app.listen(port, () => console.log(`Server up on port: ${port}`));
