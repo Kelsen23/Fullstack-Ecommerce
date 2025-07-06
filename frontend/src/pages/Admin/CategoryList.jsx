5128404dfd5001923c19749634da00d9832e3546
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-06 14:10:49 +0400

    Initialize Modal inside of CategoryList and rename state of updatedName to updatingName

--- frontend/src/pages/Admin/CategoryList.jsx
+++ frontend/src/pages/Admin/CategoryList.jsx
@@ -7,12 +7,13 @@ import {
   useFetchCategoriesQuery,
 } from "../../redux/api/categoryApiSlice";
 import CategoryForm from "../../components/CategoryForm";
+import Modal from "../../components/Modal";
 
 const CategoryList = () => {
   const { data: categories, refetch } = useFetchCategoriesQuery();
   const [name, setName] = useState("");
   const [selectedCategory, setSelectedCategory] = useState(null);
-  const [updatedName, setUpdatedName] = useState("");
+  const [updatingName, setUpdatingName] = useState("");
   const [modalVisible, setModalVisible] = useState(false);
 
   const [createCategory] = useCreateCategoryMutation();
@@ -33,7 +34,7 @@ const CategoryList = () => {
       toast.success(`Successfully created category with name of: ${res.name}`);
     } catch (error) {
       console.error(error);
-      toast.error(error?.data?.error || error?.error || "Register failed");
+      toast.error(error?.data?.error || error?.error || "Create failed");
     }
   };
 
@@ -41,7 +42,6 @@ const CategoryList = () => {
     <div className="ml-[10rem] flex flex-col md:flex-row">
       <div className="md:w-3/4 p-3">
         <div className="h-12">Manage Categories</div>
-
         <CategoryForm
           value={name}
           setValue={setName}
@@ -54,18 +54,30 @@ const CategoryList = () => {
           {categories?.map((category) => (
             <div key={category._id}>
               <button
+                className="bg-white border border-pink-500 text-pink-500 py-2 px-4 rounded-lg m-3 hover:bg-pink-500 hover:text-white focus:outline-none foucs:ring-2 focus:ring-pink-500 focus:ring-opacity-50"
                 onClick={() => {
-                  setModalVisible(true);
-                  setSelectedCategory(category);
-                  setUpdatedName(category.name);
+                  {
+                    setModalVisible(true);
+                    setSelectedCategory(category);
+                    setUpdatingName(category.name);
+                  }
                 }}
-                className="bg-white border-pink-500 text-pink-500 py-2 px-4 rounded-lg m-3 hover:bg-pink-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50"
               >
                 {category.name}
               </button>
             </div>
           ))}
         </div>
+
+        <Modal isOpen={modalVisible} onClose={() => setModalVisible(false)}>
+          <CategoryForm
+            value={updatingName}
+            setValue={(value) => setUpdatingName(value)}
+            handleSubmit={handleUpdateCategory}
+            buttonText="Update"
+            handleDelete={handleDeleteCategory}
+          />
+        </Modal>
       </div>
     </div>
   );
