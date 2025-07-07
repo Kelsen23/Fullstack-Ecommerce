ed59f1a4f364833bc5ab6f73f22d68cf0f9f91ab
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-07 16:18:03 +0400

    Add static file serving for uploaded files

--- backend/index.js
+++ backend/index.js
@@ -22,4 +22,7 @@ app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/upload", uploadRoutes);
 
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
+
 app.listen(port, () => console.log(`Server up on port: ${port}`));
