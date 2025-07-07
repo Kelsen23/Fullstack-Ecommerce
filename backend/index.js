768d95615d1878ddce3746dbbf12715bedb72a5c
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-07 15:40:34 +0400

    Fix typo in categoryRoutes import and ensure uploadRoutes is included

--- backend/index.js
+++ backend/index.js
@@ -4,8 +4,9 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
-import categotyRoutes from "./routes/categoryRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 dotenv.config({ path: path.resolve(process.cwd(), "backend/.env") });
 const port = process.env.PORT || 5000;
@@ -17,7 +18,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/api/users", userRoutes);
-app.use("/api/category", categotyRoutes);
+app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
+app.use("/api/upload", uploadRoutes);
 
 app.listen(port, () => console.log(`Server up on port: ${port}`));
