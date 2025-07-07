52ebbed841384357679e6914bdc33fd0ec854428
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-07 20:28:35 +0400

    Add productApiSlice to manage product-related API endpoints

---
+++ frontend/src/redux/api/productApiSlice.js
@@ -0,0 +1,58 @@
+import { PRODUCT_URL, UPLOAD_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+const productApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => ({
+        url: `${PRODUCT_URL}`,
+        params: { keyword },
+      }),
+      keepUnusedDataFor: 5,
+      providesTags: ["Product"],
+    }),
+
+    getProductById: builder.query({
+      query: (productId) => ({
+        url: `${PRODUCT_URL}/${productId}`,
+      }),
+      keepUnusedDataFor: 5,
+      providesTags: (result, error, productId) => [
+        { type: "Product", id: productId },
+      ],
+    }),
+
+    allProducts: builder.query({
+      query: () => ({
+        url: `${PRODUCT_URL}/allproducts`,
+      }),
+      providesTags: ["Product"],
+    }),
+
+    createProduct: builder.mutation({
+      query: (productData) => ({
+        url: `${PRODUCT_URL}`,
+        method: "POST",
+        body: productData,
+      }),
+      invalidatesTags: ["Product"],
+    }),
+
+    updateProducts: builder.mutation({
+      query: ({ productId, formData }) => ({
+        url: `${PRODUCT_URL}/${productId}`,
+        method: "PUT",
+        body: formData,
+      }),
+      invalidatesTags: ["Product"],
+    }),
+
+    deleteProduct: builder.mutation({
+      query: (productId) => ({
+        url: `${PRODUCT_URL}/${productId}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Product"],
+    }),
+  }),
+});
