16c0040ce7cff97037bd7950715c3ed68ae32591
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-07 11:02:21 +0400

    Initialize productRoutes in index

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import categotyRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config({ path: path.resolve(process.cwd(), "backend/.env") });
 const port = process.env.PORT || 5000;
@@ -17,5 +18,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use("/api/users", userRoutes);
 app.use("/api/category", categotyRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => console.log(`Server up on port: ${port}`));
