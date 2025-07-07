810a0b67fdc5f3324ae031233a3e9615677e4a8d
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-07 12:55:12 +0400

    Replace switches with findEmptyField util and add updateProduct controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,26 +1,61 @@
 import asyncHandler from "../middlewares/asyncHandler.js";
 import Product from "../models/productModel.js";
+import findEmptyField from "../utils/findEmptyField.js";
 
 const addProduct = asyncHandler(async (req, res) => {
-  const { name, description, price, category, quantity, brand } = req.fields;
+  const requiredFields = [
+    "name",
+    "image",
+    "description",
+    "price",
+    "category",
+    "quantity",
+    "brand",
+  ];
 
-  switch (true) {
-    case !name:
-      return res.json({ error: "Name is required." });
-    case !description:
-      return res.json({ error: "Description is required." });
-    case !price:
-      return res.json({ error: "Price is required." });
-    case !category:
-      return res.json({ error: "Category is required." });
-    case !quantity:
-      return res.json({ error: "Quantity is required." });
-    case !brand:
-      return res.json({ error: "Brand is required." });
+  const errorMessage = requiredFields(req.fields, requiredFields);
+  if (errorMessage) {
+    return res.status(400).json({ error: errorMessage });
   }
 
   const product = await Product.create({ ...req.fields });
   res.json(product);
 });
 
-export { addProduct };
+const updateProduct = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const requiredFields = [
+    "name",
+    "image",
+    "description",
+    "price",
+    "category",
+    "quantity",
+    "brand",
+  ];
+
+  if (!id) {
+    const error = new Error("Please provide id.");
+    error.statusCode = 400;
+    throw error;
+  }
+
+  const errorMessage = findEmptyField(req.fields, requiredFields);
+  if (errorMessage) {
+    return res.status(400).json({ error: errorMessage });
+  }
+
+  const product = await Product.findByIdAndUpdate(
+    id,
+    { ...req.fields },
+    { new: true }
+  );
+
+  if (!product) {
+    return res.status(404).json({ error: "Product not found." });
+  } else {
+    res.status(200).json(product);
+  }
+});
+
+export { addProduct, updateProduct };
