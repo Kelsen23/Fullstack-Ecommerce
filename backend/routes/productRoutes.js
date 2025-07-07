3574fc50808beb50c87e8fec30b2f3a7399fbdce
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-07 11:28:40 +0400

    Create POST method for productRoutes to create products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,4 +1,15 @@
 import express from "express";
+import formidable from "express-formidable";
 const router = express.Router();
+
+import { addProduct } from "../controllers/productController.js";
+
+import {
+  authenticate,
+  authorizedAdmin,
+} from "../middlewares/authMiddleware.js";
+import checkId from "../middlewares/checkId.js";
+
+router.route("/").post(authenticate, authorizedAdmin, formidable(), addProduct);
 
 export default router;
