aca8ee91d93fadcb026c080e89ddebef3d8e9300
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-08 13:00:45 +0400

    Update navigation link in AdminMenu to point to all products list

--- frontend/src/pages/Admin/AdminMenu.jsx
+++ frontend/src/pages/Admin/AdminMenu.jsx
@@ -68,7 +68,7 @@ const AdminMenu = () => {
                 <li>
                   <NavLink
                     className="list-item py-2 px-3 mb-5 hover:bg-[#2E2D2D] rounded-sm"
-                    to="/admin/allproducts"
+                    to="/admin/allproductslist"
                     style={({ isActive }) => ({
                       color: isActive ? "greenyellow" : "white",
                     })}
