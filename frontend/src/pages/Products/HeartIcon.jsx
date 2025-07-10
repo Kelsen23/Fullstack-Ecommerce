f7e1bf67ebb03544fbd30c8bd13557937b690dda
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-10 15:12:11 +0400

    Create HeartIcon with full logic

---
+++ frontend/src/pages/Products/HeartIcon.jsx
@@ -0,0 +1,45 @@
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addToFavorites,
+  removeFromFavorites,
+  setFavorites,
+} from "../../redux/features/favorites/favoriteSlice";
+import {
+  addFavoriteToLocalStorage,
+  removeFavoriteFromLocalStorage,
+  getFavoritesFromLocalStorage,
+} from "../../Utils/localStorage";
+import { useEffect } from "react";
+
+const HeartIcon = ({ product }) => {
+  const dispatch = useDispatch();
+  const favorites = useSelector((state) => state.favorites) || [];
+  const isFavorited = favorites.some((p) => p._id === product._id);
+
+  useEffect(() => {
+    const favoritesFromLocalStorage = getFavoritesFromLocalStorage();
+    dispatch(setFavorites(favoritesFromLocalStorage));
+  }, []);
+
+  const toggleFavorites = () => {
+    if (isFavorited) {
+      removeFavoriteFromLocalStorage(product._id);
+      dispatch(removeFromFavorites(product._id));
+    } else {
+      addFavoriteToLocalStorage(product);
+      dispatch(addToFavorites(product));
+    }
+  };
+
+  return (
+    <div
+      onClick={toggleFavorites}
+      className="absolute top-2 right-5 cursor-pointer"
+    >
+      {isFavorited ? <FaHeart className="text-pink-500" /> : <FaRegHeart />}
+    </div>
+  );
+};
+
+export default HeartIcon;
