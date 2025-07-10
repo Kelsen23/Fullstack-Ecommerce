35f190c30992f4159fbbe024075b926ddc29c5bd
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-10 12:47:10 +0400

    Implement ProductCarousel in Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,3 +1,4 @@
+import ProductCarousel from "../pages/Products/ProductCarousel";
 import SmallProduct from "../pages/Products/SmallProduct";
 import { useGetTopProductQuery } from "../redux/api/productApiSlice";
 import Loader from "./Loader";
@@ -10,14 +11,15 @@ const Header = () => {
     return <h1 className="text-red-500 text-center">Something went wrong.</h1>;
 
   return (
-    <div className="px-8 py-6">
-      <div className="hidden xl:flex justify-center">
+    <div className="px-8 py-6 flex justify-center gap-10">
+      <div className="hidden xl:flex">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-2">
           {data.map((product) => (
             <SmallProduct key={product._id} product={product} />
           ))}
         </div>
       </div>
+      <ProductCarousel />
     </div>
   );
 };
