89a0a0a7f71b493d0e2ecb452e8666b73eee2963
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-10 14:08:47 +0400

    Initialize favorites in store's reducer

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 import { apiSlice } from "./api/apiSlice";
 import authReducer from "./features/auth/authSlice";
+import favoriteReducer from "./features/favorites/favoriteSlice";
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authReducer,
+    favorites: favoriteReducer,
   },
   middleware: (getdefaultMiddleware) =>
     getdefaultMiddleware().concat(apiSlice.middleware),
