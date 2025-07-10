7ee9578aff51d12b54e1e51127dfb5306b5cb57f
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-10 13:47:30 +0400

    Get Products by keyword and render them

---
+++ frontend/src/Home.jsx
@@ -0,0 +1,49 @@
+import { Link, useParams } from "react-router-dom";
+import { useGetProductsQuery } from "./redux/api/productApiSlice";
+import Header from "./components/Header";
+import Loader from "./components/Loader";
+import Message from "./components/Message";
+import Product from "./pages/Products/Product";
+
+const Home = () => {
+  const { keyword } = useParams();
+  const { data, isLoading, error } = useGetProductsQuery({ keyword });
+
+  return (
+    <div>
+      {!keyword ? <Header /> : null}{" "}
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="error">{error?.data.message || error.error}</Message>
+      ) : (
+        <>
+          <div className="flex justify-between items-center">
+            <h1 className="ml-[20rem] mt-[10rem] text-[3rem]">
+              Special Products
+            </h1>
+
+            <Link
+              to="/shop"
+              className="bg-pink-600 text-white font-bold rounded-full py-2 px-10 mr-[18rem] mt-[1rem]"
+            >
+              Shop
+            </Link>
+          </div>
+
+          <div>
+            <div className="flex justify-center flex-wrap mt-[2rem]">
+              {data.products.map((product) => (
+                <div key={product._id}>
+                  <Product product={product} />
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Home;
