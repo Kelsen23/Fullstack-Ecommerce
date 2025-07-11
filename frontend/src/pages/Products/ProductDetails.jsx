5242d17cb37bdc0ac87434f024a9eb826756608a
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-11 13:46:48 +0400

    Add more details to product and implement ProductTabs in ProductDetails

--- frontend/src/pages/Products/ProductDetails.jsx
+++ frontend/src/pages/Products/ProductDetails.jsx
@@ -7,11 +7,20 @@ import {
 } from "../../redux/api/productApiSlice";
 import Loader from "../../components/Loader";
 import Message from "../../components/Message";
-import { FaBox, FaClock, FaShoppingCart, FaStar, FaStore } from "react-icons/fa";
+import {
+  FaBox,
+  FaClock,
+  FaShoppingCart,
+  FaStar,
+  FaStore,
+} from "react-icons/fa";
+import { MdOutlineAddShoppingCart } from "react-icons/md";
 import moment from "moment";
 import HeartIcon from "./HeartIcon";
 import { useState } from "react";
 import Product from "./Product";
+import Ratings from "./Ratings";
+import ProductTabs from "./ProductTabs";
 
 const ProductDetails = () => {
   const { _id: productId } = useParams();
@@ -54,58 +63,108 @@ const ProductDetails = () => {
           </Message>
         ) : (
           <>
-            <div className="flex w-full justify-center mt-[2rem]">
-              <div>
-                <img
-                  src={product.image}
-                  alt={product.name}
-                  className="w-[35rem] h-[25rem] mr-[2rem] rounded"
-                />
+            <div className="flex flex-col justify-center">
+              <div className="flex w-full justify-center mt-[2rem]">
+                <div>
+                  <img
+                    src={product.image}
+                    alt={product.name}
+                    className="w-[35rem] h-[25rem] mr-[2rem] rounded"
+                  />
+                </div>
+
+                <div className="flex flex-col justify-between">
+                  <div className="flex justify-between items-center">
+                    <h2 className="text-2xl font-semibold">{product.name}</h2>
+                    <HeartIcon product={product} />
+                  </div>
+
+                  <p className="my-4 xl:w-[35rem] lg:w-[35rem] md:w-[30rem]">
+                    {product.description}
+                  </p>
+                  <p className="text-5xl my-4 font-extrabold">
+                    ${product.price}
+                  </p>
+
+                  <div className="flex gap-18 items-center  justify-between mt-5 w-[20rem]">
+                    <div>
+                      <h1 className="flex whitespace-nowrap items-center mb-6">
+                        <FaStore className="mr-2" /> Brand: {product.brand}
+                      </h1>
+                      <h1 className="flex whitespace-nowrap items-center mb-6">
+                        <FaClock className="mr-2" /> Added:{" "}
+                        {moment(product.createdAt).fromNow()}
+                      </h1>
+                      <h1 className="flex whitespace-nowrap items-center mb-6">
+                        <FaStar className="mr-2" /> Reviews:{" "}
+                        {product.numReviews}
+                      </h1>
+                    </div>
+
+                    <div>
+                      <h1 className="flex whitespace-nowrap items-center mb-6">
+                        <FaStar className="mr-2" /> Ratings: {product.rating}
+                      </h1>
+
+                      <h1 className="flex whitespace-nowrap items-center mb-6">
+                        <FaShoppingCart className="mr-2" /> Quantity:{" "}
+                        {product.quantity}
+                      </h1>
+
+                      <h1 className="flex whitespace-nowrap items-center mb-6">
+                        <FaBox className="mr-2" /> Count In Stock:{" "}
+                        {product.countStock}
+                      </h1>
+                    </div>
+                  </div>
+
+                  <div className="flex justify-between flex-wrap">
+                    <Ratings
+                      value={product.rating}
+                      text={`${product.numReviews} reviews`}
+                    />
+
+                    {product.countStock > 0 && (
+                      <div>
+                        <select
+                          value={qty}
+                          onChange={(e) => setQty(e.target.value)}
+                          className="p-2 w-[6rem] rounded-md bg-white border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-pink-500 focus:border-pink-500 transition duration-150 ease-in-out text-gray-700"
+                        >
+                          {[...Array(product.countStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </select>
+                      </div>
+                    )}
+                  </div>
+
+                  <div>
+                    <button
+                      className="flex items-center gap-2 bg-pink-600 text-white py-2 px-4 rounded-lg mt-4 md:mt-0 cursor-pointer"
+                      // onClick={addToCartHandler}
+                      disabled={product.countStock < 1}
+                    >
+                      <MdOutlineAddShoppingCart size={18} />
+                      Add To Cart
+                    </button>
+                  </div>
+                </div>
               </div>
-              <div className="flex flex-col justify-between">
-                <div className="flex justify-between items-center">
-                  <h2 className="text-2xl font-semibold">{product.name}</h2>
-                  <HeartIcon product={product} />
-                </div>
 
-                <p className="my-4 xl:w-[35rem] lg:w-[35rem] md:w-[30rem]">
-                  {product.description}
-                </p>
-                <p className="text-5xl my-4 font-extrabold">${product.price}</p>
-
-                <div className="flex items-center justify-between mt-5 w-[20rem]">
-                  <div>
-                    <h1 className="flex items-center mb-6">
-                      <FaStore className="mr-2" /> Brand: {product.brand}
-                    </h1>
-                    <h1 className="flex items-center mb-6">
-                      <FaClock className="mr-2" /> Added:{" "}
-                      {moment(product.createdAt).fromNow()}
-                    </h1>
-                    <h1 className="flex items-center mb-6">
-                      <FaStar className="mr-2" /> Reviews: {product.numReviews}
-                    </h1>
-                  </div>
-
-                  <div>
-                    <h1 className="flex items-center mb-6">
-                      <FaStar className="mr-2" /> Ratings: {product.rating}
-                    </h1>
-
-                    <h1 className="flex items-center mb-6">
-                      <FaShoppingCart className="mr-2" /> Quantity: {product.quantity}
-                    </h1>
-
-                    <h1 className="flex items-center mb-6">
-                      <FaBox className="mr-2" /> Count In Stock: {product.countStock}
-                    </h1>
-                  </div>
-                </div>
-
-                <div className="flex justify-between flex-wrap">
-
-                </div>
-
+              <div className="mt-[5rem] flex flex-wrap items-start justify-between ml-[10rem]">
+                <ProductTabs
+                  loadingProductReview={loadingProductReview}
+                  userInfo={userInfo}
+                  // submitHandler={submitHandler}
+                  rating={rating}
+                  setRating={setRating}
+                  comment={comment}
+                  setComment={setComment}
+                  product={product}
+                />
               </div>
             </div>
           </>
