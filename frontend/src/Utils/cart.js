fe6b5ef201702c75f1c1a0302ec82e07ec5ff885
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-13 10:15:30 +0400

    Fix incorrect structure of the code in updateCart function

--- frontend/src/Utils/cart.js
+++ frontend/src/Utils/cart.js
@@ -6,15 +6,18 @@ export const updateCart = (state) => {
   state.itemsPrice = addDecimals(
     state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
   );
+
+  state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+
+  state.taxPrice = addDecimals(0.15 * state.itemsPrice);
+
+  state.totalPrice = addDecimals(
+    Number(state.itemsPrice) +
+      Number(state.shippingPrice) +
+      Number(state.taxPrice)
+  );
+
+  localStorage.setItem("cart", JSON.stringify(state));
+
+  return state;
 };
-
-state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
-state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)));
-state.totalPrice = (
-  Number(state.itemsPrice) +
-  Number(state.shippingPrice) +
-  Number(state.taxPrice)
-).toFixed(2);
-
-localStorage.setItem("cart", JSON.stringify(state));
-return state;
