f0d6393f25c69377da0d7920a99d3e387f6436d4
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-15 11:01:34 +0400

    Add route to mark order as delivered

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -14,6 +14,7 @@ import {
   calculateTotalSalesByDate,
   findOrderById,
   markOrderAsPaid,
+  markOrderAsDelivered
 } from "../controllers/orderController.js";
 
 router
@@ -26,5 +27,6 @@ router.route("/total-sales").get(calculateTotalSales);
 router.route("/total-sales-by-date").get(calculateTotalSalesByDate);
 router.route("/:id").get(authenticate, authorizedAdmin, findOrderById);
 router.route("/:id/pay").put(authenticate, authorizedAdmin, markOrderAsPaid);
+router.route("/:id/deliver").put(authenticate, authorizedAdmin, markOrderAsDelivered)
 
 export default router;
