3a5a230b82333c94e0a5ab84f1c8b4ca239064b9
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-16 10:41:26 +0400

    Create Shipping for handling Shipping and gathering address details

---
+++ frontend/src/pages/Orders/Shipping.jsx
@@ -0,0 +1,129 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import {
+  saveShippingAddress,
+  savePaymentMethod,
+} from "../../redux/features/cart/cartSlice";
+import ProgressSteps from "../../components/ProgressSteps";
+
+const Shipping = () => {
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  const [paymentMethod, setPaymentlMethod] = useState("PayPal");
+  const [address, setAddress] = useState(shippingAddress.address || "");
+  const [city, setCity] = useState(shippingAddress.city || "");
+  const [postalCode, setPostalCode] = useState(
+    shippingAddress.postalCode || ""
+  );
+  const [country, setCountry] = useState(shippingAddress.country || "");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!shippingAddress.address) {
+      navigate("/shipping");
+    }
+  }, [navigate, shippingAddress]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(saveShippingAddress({ address, city, postalCode, country }));
+    dispatch(savePaymentMethod(paymentMethod));
+    navigate("/placeholder");
+  };
+
+  return (
+    <div className="container mx-auto mt-10">
+      <ProgressSteps step1 step2 />
+      <div className="div mt-[10rem] flex justify-around items-center flex-wrap">
+        <form onSubmit={handleSubmit} className="w-[40rem]">
+          <h1 className="text-2xl font-semibold mb-4">Shipping</h1>
+
+          <div className="mb-4">
+            <label className="block mb-2">Address</label>
+
+            <input
+              type="text"
+              className="block w-full p-2 border rounded"
+              placeholder="Enter Address"
+              value={address}
+              required
+              onChange={(e) => setAddress(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block mb-2">City</label>
+
+            <input
+              type="text"
+              className="block w-full p-2 border rounded"
+              placeholder="Enter City"
+              value={city}
+              required
+              onChange={(e) => setCity(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block mb-2">Postal Code</label>
+
+            <input
+              type="text"
+              className="block w-full p-2 border rounded"
+              placeholder="Enter Postal Code"
+              value={postalCode}
+              required
+              onChange={(e) => setPostalCode(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block mb-2">Address</label>
+
+            <input
+              type="text"
+              className="block w-full p-2 border rounded"
+              placeholder="Enter Country"
+              value={country}
+              required
+              onChange={(e) => setCountry(e.target.value)}
+            />
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-gray-400">Select Method</label>
+
+            <div className="mt-2">
+              <label htmlFor="" className="inline-flex items-center">
+                <input
+                  type="radio"
+                  className="font-radio accent-pink-500"
+                  name="paymentMethod"
+                  value="PayPal"
+                  checked={paymentMethod === "PayPal"}
+                  onChange={(e) => setPaymentlMethod(e.target.value)}
+                />
+
+                <span className="ml-2">PayPal or Credit Card</span>
+              </label>
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            className="bg-pink-500 text-white rounded-full py-2 px-4 text-lg w-full"
+          >
+            Continue
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Shipping;
