5bb70120180d08ace8e3ae52b942497e018d4445
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-16 13:16:27 +0400

    Refactor orderRoutes

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -14,7 +14,7 @@ import {
   calculateTotalSalesByDate,
   findOrderById,
   markOrderAsPaid,
-  markOrderAsDelivered
+  markOrderAsDelivered,
 } from "../controllers/orderController.js";
 
 router
@@ -27,6 +27,8 @@ router.route("/total-sales").get(calculateTotalSales);
 router.route("/total-sales-by-date").get(calculateTotalSalesByDate);
 router.route("/:id").get(authenticate, authorizedAdmin, findOrderById);
 router.route("/:id/pay").put(authenticate, authorizedAdmin, markOrderAsPaid);
-router.route("/:id/deliver").put(authenticate, authorizedAdmin, markOrderAsDelivered)
+router
+  .route("/:id/deliver")
+  .put(authenticate, authorizedAdmin, markOrderAsDelivered);
 
 export default router;
