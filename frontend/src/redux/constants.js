d772ef9383cd5df404061097a72010aadb735854
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-17 15:15:53 +0400

    Update constants

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,6 +1,7 @@
-export const BASE_URL = "";
-export const USERS_URL = "/api/users";
-export const CATEGORY_URL = "/api/category";
-export const PRODUCT_URL = "/api/products";
-export const UPLOAD_URL = "/api/upload";
-export const ORDER_URL = "/api/orders";
+export const BASE_URL = import.meta.env.VITE_BASE_URL || "";
+
+export const USERS_URL = `${BASE_URL}/api/users`;
+export const CATEGORY_URL = `${BASE_URL}/api/category`;
+export const PRODUCT_URL = `${BASE_URL}/api/products`;
+export const UPLOAD_URL = `${BASE_URL}/api/upload`;
+export const ORDER_URL = `${BASE_URL}/api/orders`;      
