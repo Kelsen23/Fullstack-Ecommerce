091bcbe74f4aa5625534fb9fa23cf73ba321fa60
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-19 14:06:56 +0400

    Fix sameSite and maxAge when creating a token

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -8,8 +8,8 @@ const generateToken = (res, userId) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
-    maxAge: 30 * 24 * 60 * 60 * 1000,
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    maxAge: 15 * 24 * 60 * 60 * 1000,
   });
 
   return token;
