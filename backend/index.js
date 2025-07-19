258aab69e585d63264f5cce16cedcbc40c7aa6ea
Kelsen23 <nikolozigoreziani@gmail.com>
2025-07-19 15:02:30 +0400

    Specify deployed frontend url to cors for it to allow another origin

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://fullstack-ecommerce-frontend-l5qg.onrender.com"],
     credentials: true,
   })
 );
